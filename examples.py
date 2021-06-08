c625644dfe0d9dd1e254cb29834645ca3e9ea7cf
ok97465 <ok97465@gmail.com>
2021-06-08 21:12:48 +0900

    fix pep8

--- examples.py
+++ examples.py
@@ -2,7 +2,7 @@ import matplotlib
 matplotlib.use('Qt5Agg')   # generate postscript output by default
 import pyqtgraph as pg
 from matplotlib.pyplot import imread, ion, plot, close
-from plot_qt import imagescqt, plotqt
+from plot_pg import imagescpg, plotpg
 from numpy import arange, mean, linspace
 from numpy.random import randn
 
@@ -16,7 +16,7 @@ def plot_image():
 
     x = linspace(-54.7321, 5812.512, num=data.shape[1])
     y = arange(data.shape[0]) * 1
-    imv_color = imagescqt(
+    imv_color = imagescpg(
         x, y, data, colormap='jet', title='Color Image', xlabel='Column',
         ylabel='Row', colorbar=True)
 
@@ -24,7 +24,7 @@ def plot_image():
 
     x = arange(data.shape[1]) * 1
     y = arange(data.shape[0]) * 1
-    imv_gray = imagescqt(
+    imv_gray = imagescpg(
         x, y, data, colormap='viridis',
         title='Gray Image with viridis Colormap', xlabel='Column',
         ylabel='Row', colorbar=True)
@@ -33,16 +33,16 @@ def plot_image():
     x = arange(data.shape[1]) * 5
     y = arange(data.shape[0]) * 1
 
-    imv_gray0 = imagescqt(
+    imv_gray0 = imagescpg(
         x, y, data,
         colormap='gray', title='Gray Image with gray Colormap',
         xlabel='Column', ylabel='Row', colorbar=True)
 
-    imv_gray1 = imagescqt(
+    imv_gray1 = imagescpg(
         data,
         'parula', 'Gray Image with parula Colormap', 'Column', 'Row', False)
 
-    imv_gray2 = imagescqt(
+    imv_gray2 = imagescpg(
         x, y, data,
         'jet', 'Gray Image with jet Colormap', 'Column', 'Row', True,
         {'font_family': 'Malgun Gothic', 'title_font_size': '17pt',
@@ -58,11 +58,11 @@ def plot_1d():
     n_data = 32768
     x = linspace(-5812.7321, 5812.512, num=n_data)
     y = linspace(-5812.7321, 5812.512, num=n_data)
-    fig, ax = plotqt(x, y, title='title', xlabel='xlabel', ylabel='ylabel',
+    fig, ax = plotpg(x, y, title='title', xlabel='xlabel', ylabel='ylabel',
                      name='0')
 
     for ii in range(1, 7):
-        plotqt(x - 500 * ii, y + 500 + ii,
+        plotpg(x - 500 * ii, y + 500 + ii,
                name=f'{ii}', ax=ax)
 
     return fig, ax
